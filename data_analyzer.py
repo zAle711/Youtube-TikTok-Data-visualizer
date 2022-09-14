0474cfa6802c2a9c7e97353567270ca8525b9174
zAle711 <alessandro7111@gmail.com>
2022-09-14 17:29:15 +0200

    Fixed animation function Every plot will be saved in 'img' folder.

--- data_analyzer.py
+++ data_analyzer.py
@@ -1,16 +1,19 @@
 from abc import abstractmethod, ABC
-from timeit import repeat
-from turtle import update, width
-import numpy as np
 from datetime import datetime
 import json
 import pandas as pd
 import matplotlib.pyplot as plot
-from matplotlib.animation import ArtistAnimation, FuncAnimation, FFMpegWriter
+from matplotlib.animation import FuncAnimation, FFMpegWriter
 from dateutil.relativedelta import relativedelta
+import os
+import sys
+
 class DataAnalyzer(ABC):
-    data_frame = None
+    data_frame: pd.DataFrame = None
     def __init__(self) -> None:
+
+        if not os.path.isdir('img'):
+            os.mkdir('img')
         super().__init__()
 
     @abstractmethod
@@ -26,29 +29,46 @@ class DataAnalyzer(ABC):
         return self.data_frame
     
     #PLOT NUMBERS OF VIDEO SAW BY TIME OF THE DAY (every hour)
-    def show_hours_views(self):
+    def show_views_by_hours(self):
         #Function to select 
         def set_hours(hour):
-            hour = str(hour)
-            if len(hour) == 1:
-                hour = "0" + hour
-            hour += ":00"
-            return hour
+            if 'nan' != str(hour):
+                hour = str(hour)
+                hour.replace('.', ':')
+                hour = '0' + hour + '0' if len(hour) == 3 else hour + '0'
+                return hour
 
         df = self.data_frame.copy()
         df['timestamp'] = df['timestamp'].dt.hour
         df['timestamp'] = df['timestamp'].apply(set_hours)
         
         df = df.groupby("timestamp").size().reset_index(name='count')
-        df.plot.bar(x="timestamp", y="count")
-        plot.show(block=True)
+        df.plot.bar(x="timestamp", y="count", title="Most Viewed Channels", fontsize=10)
+        plot.tight_layout()
+        plot.savefig(self.get_file_name('views_by_hours'))
+        #plot.show(block=True)
     
     #PLOT Total of video watched every day
-    def show_video_by_day(self):
-        df = self.data_frame.groupby('Date').size().reset_index(name='total_of_the_day')
-        df[ df['Date'] > datetime(2021,1,1,0,0,0).date() ].plot(x='Date', y='total_of_the_day')
-        plot.show()
+    def show_views_by_day(self):
+        #Count video watched every day
+        df = self.data_frame.groupby('date').size().reset_index(name='total_of_the_day')
+
+        #Add rows with empty days with 0 as total videos watched
+        dtr = pd.date_range(df['date'].min(), df['date'].max(), freq='D').to_frame(name='date')
+        dtr['total_of_the_day'] = 0
+
+        df.merge(dtr, how='right', on='date').fillna(0).pop('total_of_the_day_y')
+
+        
+        
+        df.plot(x='date', y='total_of_the_day')
+        plot.tight_layout()
+        plot.savefig(self.get_file_name('views_by_day_alltime.png'))
     
+    @abstractmethod
+    def get_file_name(self, name):
+        pass
+
     @abstractmethod
     def to_csv(self):
         pass
@@ -60,38 +80,44 @@ class YoutubeAnalizer(DataAnalyzer):
         self.create_dataframe()
 
     def load_data(self):
+        if not os.path.isfile('data/youtube.json'):
+            print("Place your youtube.json in a folder named: data")
+            sys.exit(0)
         with open(f"data/{self.FILE_NAME}", "r", encoding="utf-8") as fp:
             file_content = fp.read()
             fp.close()
             return json.loads(file_content)
 
     def to_csv(self):
-        self.data_frame.to_csv(f"{self.FILE_NAME[:-5]}.csv")
+        self.data_frame.to_csv(f"data/{self.FILE_NAME[:-5]}.csv")
+
+    
     #PARSE JSON AND CREATE A DATA FRAME OBJECT
     def create_dataframe(self):
+
+        #translate italian months to english needed to convert timestamp to pd.DateTime    
+        def convert_time(timestamp:str):
+        
+            MONTH_CODE = {'gen': 'Jan', 'feb': 'Feb', 'mar': 'Mar', 'apr': 'Apr', 'mag': 'May', 'giu': 'Jun', 'lug': 'Jul', 'ago': 'Aug', 'set': 'Sep', 'ott': 'Oct', 'nov': 'Nov', 'dic': 'Dec'}
+
+            date, time = timestamp.split(",")
+            month = date.split(" ")[1]
+            
+            if month in MONTH_CODE.keys() and not month[0].isdigit():
+                date = date.replace(month, MONTH_CODE[month])
+            else:
+                return None
+            date = date.replace(" ", "").strip()
+            time = time[:-4].strip()
+
+            return date + " " + time
+        
         df = pd.DataFrame(self.load_data())
-        df['timestamp'] = df['timestamp'].apply(self.convert_time)
+        df['timestamp'] = df['timestamp'].apply(convert_time)
         df['timestamp'] = pd.to_datetime( df['timestamp'], format='%d%b%Y %H:%M:%S', errors='coerce')
         df['date'] = pd.to_datetime( df['timestamp'].dt.date, format='%Y-%m-%d')
-        #df['Date'] = df['timestamp'].dt.date
         self.data_frame = df
     
-    #translate italian months to english needed to convert timestamp to pd.DateTime    
-    def convert_time(self, timestamp:str):
-        
-        MONTH_CODE = {'gen': 'Jan', 'feb': 'Feb', 'mar': 'Mar', 'apr': 'Apr', 'mag': 'May', 'giu': 'Jun', 'lug': 'Jul', 'ago': 'Aug', 'set': 'Sep', 'ott': 'Oct', 'nov': 'Nov', 'dic': 'Dec'}
-
-        date, time = timestamp.split(",")
-        month = date.split(" ")[1]
-        
-        if month in MONTH_CODE.keys() and not month[0].isdigit():
-            date = date.replace(month, MONTH_CODE[month])
-        else:
-            return None
-        date = date.replace(" ", "").strip()
-        time = time[:-4].strip()
-
-        return date + " " + time
 
     #BAR PLOT OF THE MOST VIEWED CHANNELS
     def show_barplot(self):
@@ -99,49 +125,54 @@ class YoutubeAnalizer(DataAnalyzer):
         df = self.data_frame.groupby('channel')['channel'].count().reset_index(name='views')
         df = df[df['views'] > 20]
         df = df.sort_values('views', ascending=False)
-        df.head(20).plot.bar(x="channel", y="views", title="Most Viewed Channels", figsize=(50, 50))
-        plot.subplots_adjust(bottom=.3)
-        plot.show(block=True)
+        df.head(20).plot.barh(x="channel", y="views", title="Most Viewed Channels", fontsize=10)
+        plot.ylabel('Channel name', fontdict={'fontsize':15}, fontsize=15)
+        plot.tight_layout()
+        plot.savefig(self.get_file_name('most_viewed_channel'))
+        #plot.show(block=True)
     
+    #PLOT ANIMATION OF THE MOST VIEWED CHANNEL EVERY MOUNT (CUMULATIVE)
     def animated_plot(self): 
 
         def filter_data(df):
             all_data = []
+            all_time = []
             start = datetime(2013, 4, 10)
             end = self.data_frame['date'].max()
 
             while start <= end:
-                print(start)
+                all_time.append(start)
                 query = df[ df['date'] < start ].groupby('channel')['channel'].count().reset_index(name='total').sort_values('total', ascending=False).head(20)
                 all_data.append(query)
                 start = start + relativedelta(months=1)
 
-            return all_data
-        
-
+            return all_data, all_time
         
         def get_data(n):
             return all_df[n]
 
-        all_df = filter_data(self.data_frame)
-        print(all_df[50])
+        all_df, all_time = filter_data(self.data_frame)
 
-        fig = plot.figure()
+        fig = plot.figure(figsize=(10,5))
         first_data = get_data(0)
         plot.barh(first_data['channel'], first_data['total'])
+        plot.tight_layout()
         
         def animate(i):
-            if i == len(all_df):
-                return
             plot.cla()
-            next_data = get_data(i)
+            next_data = all_df[i]
             plot.gca().barh(next_data['channel'], next_data['total'])
+            plot.tight_layout()
+            plot.suptitle(all_time[i].strftime("%Y-%m-%d"), fontsize=20)
         
-        animation = FuncAnimation(fig, animate, frames=len(all_df), repeat=False, interval=100)
-        FFwriter = FFMpegWriter(fps=30)
-        animation.save('test.mp4', writer=FFwriter)
+        animation = FuncAnimation(fig, animate, frames=len(all_df), repeat=False, interval=200)
+        FFwriter = FFMpegWriter(fps=4)
+        animation.save('img/most_wieved_channels.mp4', writer=FFwriter)
         
-        plot.show()
+        
+
+    def get_file_name(self, name):
+        return f"img/yt_{name}.png"
 
 
 class TikTokAnalyzer(DataAnalyzer):
@@ -150,13 +181,16 @@ class TikTokAnalyzer(DataAnalyzer):
         self.create_dataframe()
     
     def load_data(self):
+        if not os.path.isfile('data/tiktok.json'):
+            print("Place your tiktok.json in a folder named: data")
+            sys.exit(0)
         with open('data/tiktok.json', 'r', encoding='utf-8') as file:
             data = json.loads(file.read())       
             file.close()
             return data['Activity']['Video Browsing History']['VideoList']
     
     def to_csv(self):
-        self.data_frame.to_csv(f"{self.FILE_NAME[:-5]}.csv")
+        self.data_frame.to_csv(f"data/{self.FILE_NAME[:-5]}.csv")
 
     def create_dataframe(self):
         df = pd.json_normalize(self.load_data())
@@ -166,19 +200,20 @@ class TikTokAnalyzer(DataAnalyzer):
         df = df.dropna()
         self.data_frame = df
 
+    def get_file_name(self, name):
+        return f"img/{name}.png"
 
 if __name__ == "__main__":
-    yt = YoutubeAnalizer()
-    yt.to_csv()
+    yt = YoutubeAnalizer()    
     tiktok = TikTokAnalyzer()
-    # yt.show_barplot()
-    # yt.show_hours_views()
-    #yt.show_video_by_day()
-    #tiktok.show_hours_views()
-    #tiktok.show_video_by_day()
+    
+    tiktok.show_views_by_hours()
+    tiktok.show_views_by_day()
+
+    yt.show_barplot()
+    yt.show_views_by_hours()
+    yt.show_views_by_day()    
     yt.animated_plot()
 
-    
-
-        
-    
+    yt.to_csv()
+    tiktok.to_csv()
