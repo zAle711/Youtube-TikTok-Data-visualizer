fb9776fded29b10faa179b11e950c181e0ece9bd
zAle711 <alessandro7111@gmail.com>
2022-09-14 13:50:31 +0200

    Added a function to download dataframe in csv and made a function to animate the most wieved channel every month

--- data_analyzer.py
+++ data_analyzer.py
@@ -1,9 +1,13 @@
 from abc import abstractmethod, ABC
+from timeit import repeat
+from turtle import update, width
+import numpy as np
 from datetime import datetime
 import json
 import pandas as pd
 import matplotlib.pyplot as plot
-
+from matplotlib.animation import ArtistAnimation, FuncAnimation, FFMpegWriter
+from dateutil.relativedelta import relativedelta
 class DataAnalyzer(ABC):
     data_frame = None
     def __init__(self) -> None:
@@ -44,6 +48,10 @@ class DataAnalyzer(ABC):
         df = self.data_frame.groupby('Date').size().reset_index(name='total_of_the_day')
         df[ df['Date'] > datetime(2021,1,1,0,0,0).date() ].plot(x='Date', y='total_of_the_day')
         plot.show()
+    
+    @abstractmethod
+    def to_csv(self):
+        pass
 
 class YoutubeAnalizer(DataAnalyzer):
     FILE_NAME = 'youtube.json'
@@ -57,12 +65,15 @@ class YoutubeAnalizer(DataAnalyzer):
             fp.close()
             return json.loads(file_content)
 
+    def to_csv(self):
+        self.data_frame.to_csv(f"{self.FILE_NAME[:-5]}.csv")
     #PARSE JSON AND CREATE A DATA FRAME OBJECT
     def create_dataframe(self):
         df = pd.DataFrame(self.load_data())
         df['timestamp'] = df['timestamp'].apply(self.convert_time)
         df['timestamp'] = pd.to_datetime( df['timestamp'], format='%d%b%Y %H:%M:%S', errors='coerce')
-        df['Date'] = df['timestamp'].dt.date
+        df['date'] = pd.to_datetime( df['timestamp'].dt.date, format='%Y-%m-%d')
+        #df['Date'] = df['timestamp'].dt.date
         self.data_frame = df
     
     #translate italian months to english needed to convert timestamp to pd.DateTime    
@@ -92,8 +103,47 @@ class YoutubeAnalizer(DataAnalyzer):
         plot.subplots_adjust(bottom=.3)
         plot.show(block=True)
     
-    
+    def animated_plot(self): 
+
+        def filter_data(df):
+            all_data = []
+            start = datetime(2013, 4, 10)
+            end = self.data_frame['date'].max()
+
+            while start <= end:
+                print(start)
+                query = df[ df['date'] < start ].groupby('channel')['channel'].count().reset_index(name='total').sort_values('total', ascending=False).head(20)
+                all_data.append(query)
+                start = start + relativedelta(months=1)
+
+            return all_data
         
+
+        
+        def get_data(n):
+            return all_df[n]
+
+        all_df = filter_data(self.data_frame)
+        print(all_df[50])
+
+        fig = plot.figure()
+        first_data = get_data(0)
+        plot.barh(first_data['channel'], first_data['total'])
+        
+        def animate(i):
+            if i == len(all_df):
+                return
+            plot.cla()
+            next_data = get_data(i)
+            plot.gca().barh(next_data['channel'], next_data['total'])
+        
+        animation = FuncAnimation(fig, animate, frames=len(all_df), repeat=False, interval=100)
+        FFwriter = FFMpegWriter(fps=30)
+        animation.save('test.mp4', writer=FFwriter)
+        
+        plot.show()
+
+
 class TikTokAnalyzer(DataAnalyzer):
     FILE_NAME = 'tiktok.json'
     def __init__(self, ) -> None:
@@ -104,45 +154,29 @@ class TikTokAnalyzer(DataAnalyzer):
             data = json.loads(file.read())       
             file.close()
             return data['Activity']['Video Browsing History']['VideoList']
+    
+    def to_csv(self):
+        self.data_frame.to_csv(f"{self.FILE_NAME[:-5]}.csv")
 
     def create_dataframe(self):
         df = pd.json_normalize(self.load_data())
         df = df.rename({'Date':'timestamp'}, axis='columns')
         df['timestamp'] = pd.to_datetime( df['timestamp'], format="%Y-%m-%d %H:%M:%S")
-        df['Date'] = df['timestamp'].dt.date
+        df['date'] = pd.to_datetime( df['timestamp'].dt.date, format='%Y-%m-%d')
         df = df.dropna()
         self.data_frame = df
 
 
-
-
-
-
-# def create_animated_dataframe(data_frame):
-#     new_data_frame = data_frame.sort_values('timestamp')
-#     #adding date info to column
-#     new_data_frame['only_date'] = pd.to_datetime(new_data_frame['timestamp'].dt.strftime('%d%b%Y'), format='%d%b%Y')
-    
-#     #counting number of video watched in a day
-#     new_data_frame = new_data_frame.groupby(['channel', 'only_date']).size().reset_index(name="count").sort_values( ['channel', 'only_date'],ascending=True)
-#     #counting number of video watched in a month
-#     new_data_frame['only_date'] = pd.to_datetime(new_data_frame['only_date'].dt.strftime('%b%Y'), format='%b%Y')
-#     new_data_frame = new_data_frame.groupby(['channel', 'only_date']).size().reset_index(name="count").sort_values( ['channel', 'only_date'],ascending=True)
-#     #adding cumulative number of videos
-#     new_data_frame['cumulative'] = new_data_frame.groupby(['channel']).cumsum()
-    
-
-# def create_animated_plot(data_frame):
-#     data_frame.plot_animated('tets.gif', period_fmt="%b%Y", title="ciao")
-
 if __name__ == "__main__":
     yt = YoutubeAnalizer()
+    yt.to_csv()
     tiktok = TikTokAnalyzer()
     # yt.show_barplot()
     # yt.show_hours_views()
-    yt.show_video_by_day()
+    #yt.show_video_by_day()
     #tiktok.show_hours_views()
     #tiktok.show_video_by_day()
+    yt.animated_plot()
 
     
 
